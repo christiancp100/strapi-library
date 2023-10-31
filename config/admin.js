4d036959f36db626bb72322c0b7abf2162a3fb4e
Christian Capeans <christiancp100@gmail.com>
2023-10-31 09:14:47 +0100

    update: strapi version

--- config/admin.js
+++ config/admin.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env("ADMIN_JWT_SECRET", "aaaaaaaaaaa"),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env("API_TOKEN_SALT", "aaaaaaaaaaa"),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env("TRANSFER_TOKEN_SALT", "aaaaaaaaaaa"),
     },
   },
 });
