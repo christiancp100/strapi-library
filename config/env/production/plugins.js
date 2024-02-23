c7a45ffac912302b4d1abf40d654eebf88a26fcf
Christian Capeans <christiancp100@gmail.com>
2024-02-23 14:39:41 +0100

    add production aws provider

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -8,10 +8,10 @@ module.exports = ({ env }) => ({
           accessKeyId: env("AWS_ACCESS_KEY_ID"),
           secretAccessKey: env("AWS_ACCESS_SECRET"),
           region: env("AWS_REGION"),
-          // endpoint: env(
-          //   "AWS_S3_ENDPOINT_URL",
-          //   "https://strapi-assets-transfer.s3.eu-west-1.amazonaws.com/"
-          // ),
+          endpoint: env(
+            "AWS_S3_ENDPOINT_URL",
+            "https://strapi-assets-transfer.s3.eu-west-1.amazonaws.com/"
+          ),
           params: {
             ACL: env("AWS_ACL", "public-read"),
             signedUrlExpires: env("AWS_SIGNED_URL_EXPIRES", 15 * 60),
