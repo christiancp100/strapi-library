9ecf74055e2fde1d2836bf964b468262469f6461
Christian Capeans <christiancp100@gmail.com>
2024-02-23 14:11:28 +0100

    update experimental

--- config/plugins.js
+++ config/plugins.js
@@ -1,30 +1,30 @@
 module.exports = ({ env }) => ({
-  upload: {
-    config: {
-      sizeLimit: 25 * 1024 * 1024 * 1024,
-      provider: "aws-s3",
-      providerOptions: {
-        s3Options: {
-          accessKeyId: env("AWS_ACCESS_KEY_ID"),
-          secretAccessKey: env("AWS_ACCESS_SECRET"),
-          region: env("AWS_REGION"),
-          // endpoint: env(
-          //   "AWS_S3_ENDPOINT_URL",
-          //   "https://strapi-assets-transfer.s3.eu-west-1.amazonaws.com/"
-          // ),
-          params: {
-            ACL: env("AWS_ACL", "public-read"),
-            signedUrlExpires: env("AWS_SIGNED_URL_EXPIRES", 15 * 60),
-            Bucket: env("AWS_BUCKET"),
-          },
-          s3ForcePathStyle: true,
-        },
-      },
-      actionOptions: {
-        upload: {},
-        uploadStream: {},
-        delete: {},
-      },
-    },
-  },
+  // upload: {
+  //   config: {
+  //     sizeLimit: 25 * 1024 * 1024 * 1024,
+  //     provider: "aws-s3",
+  //     providerOptions: {
+  //       s3Options: {
+  //         accessKeyId: env("AWS_ACCESS_KEY_ID"),
+  //         secretAccessKey: env("AWS_ACCESS_SECRET"),
+  //         region: env("AWS_REGION"),
+  //         // endpoint: env(
+  //         //   "AWS_S3_ENDPOINT_URL",
+  //         //   "https://strapi-assets-transfer.s3.eu-west-1.amazonaws.com/"
+  //         // ),
+  //         params: {
+  //           ACL: env("AWS_ACL", "public-read"),
+  //           signedUrlExpires: env("AWS_SIGNED_URL_EXPIRES", 15 * 60),
+  //           Bucket: env("AWS_BUCKET"),
+  //         },
+  //         s3ForcePathStyle: true,
+  //       },
+  //     },
+  //     actionOptions: {
+  //       upload: {},
+  //       uploadStream: {},
+  //       delete: {},
+  //     },
+  //   },
+  // },
 });
--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,32 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface BasicHeading extends Schema.Component {
+  collectionName: 'components_basic_headings';
+  info: {
+    displayName: 'heading';
+    icon: 'bulletList';
+  };
+  attributes: {
+    Footer: Attribute.String;
+    title: Attribute.String;
+  };
+}
+
+export interface BasicNewcomp extends Schema.Component {
+  collectionName: 'components_basic_newcomps';
+  info: {
+    displayName: 'newcomp';
+  };
+  attributes: {
+    h1: Attribute.String;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'basic.heading': BasicHeading;
+      'basic.newcomp': BasicNewcomp;
+    }
+  }
 }
